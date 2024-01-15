7bbcd413358ce635e5f275b1b61517ce273fa025
jmwamser <jwamser@redpandacoding.com>
2024-01-15 02:07:16 +0000

    add all libs to workflow

---
+++ ExceptionInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+/*******************************************************************************
+ * Auburnite
+ *
+ * @link                https://github.com/Auburnite/Auburnite
+ * @copywrite           Copywrite (c) 2023-present | Jordan Wamser - RedPanda Coding
+ * @license             https://github.com/Auburnite/Auburnite/blob/main/LICENSE
+ ******************************************************************************/
+namespace Auburnite\Auburnite\Contracts\Exception;
+
+interface ExceptionInterface extends \Throwable
+{
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Auburnite Exception Contracts
+
+Core exception interface for all Auburnite Components, Bundles, and Contracts.
+
+See https://github.com/Auburnite/Auburnite/blob/0.1/README.md for more information.
